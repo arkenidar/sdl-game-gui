3d0c46ae202c323a5ad75d7713982731520c9832
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-12-27 23:12:29 +0100

    add script to copy necessary DLLs for SDL2 and SDL2_ttf

---
+++ copy-dlls.bash
@@ -0,0 +1,24 @@
+
+drive_win_msys=/c/msys64
+drive_win_gnulinux=/mnt/ntfs/msys64
+
+drive_win=$drive_win_gnulinux
+# drive_win=$drive_win_msys
+
+cp $drive_win/mingw64/bin/SDL2.dll .
+cp $drive_win/mingw64/bin/libgcc_s_seh-1.dll .
+cp $drive_win/mingw64/bin/libstdc++-6.dll .
+cp $drive_win/mingw64/bin/libwinpthread-1.dll .
+
+cp $drive_win/mingw64/bin/SDL2_ttf.dll .
+cp $drive_win/mingw64/bin/libfreetype-6.dll .
+cp $drive_win/mingw64/bin/libbrotlidec.dll .
+cp $drive_win/mingw64/bin/libbrotlicommon.dll .
+cp $drive_win/mingw64/bin/libbz2-1.dll .
+cp $drive_win/mingw64/bin/libharfbuzz-0.dll .
+cp $drive_win/mingw64/bin/libglib-2.0-0.dll .
+cp $drive_win/mingw64/bin/libintl-8.dll .
+cp $drive_win/mingw64/bin/libiconv-2.dll .
+cp $drive_win/mingw64/bin/libpcre2-8-0.dll .
+cp $drive_win/mingw64/bin/libgraphite2.dll .
+cp $drive_win/mingw64/bin/libpng16-16.dll .
